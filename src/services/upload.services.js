574c41de8e3dd8b16584443599d903daef2b3edb
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2023-04-28 12:21:38 +0200

    uploadAudioService done

--- src/services/upload.services.js
+++ src/services/upload.services.js
@@ -3,6 +3,11 @@ import service from "./config.services";
 const uploadPictureService = (pictureFile) => {
     return service.post("/upload",pictureFile)
    }
+
+const uploadAudioService = (audioFile) => {
+    return service.post("upload/uploadAudio", audioFile)
+}
    export {
-       uploadPictureService
+       uploadPictureService,
+       uploadAudioService
    }
