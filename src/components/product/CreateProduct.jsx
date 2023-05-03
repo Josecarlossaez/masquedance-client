d7a8e33f4b8e25e119499b29ff90bd809cf75d7e
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2023-05-03 17:23:10 +0200

    create product changed with reference

--- src/components/product/CreateProduct.jsx
+++ src/components/product/CreateProduct.jsx
@@ -37,15 +37,14 @@ function CreateProduct() {
   // input values
    const [nameInput, setName] = useState("");
    const [priceInput, setPrice] = useState("");
-  //  const [sizeInput, setSize] = useState("");
+   const [sizeInput, setSize] = useState("");
    const [descriptionInput, setDescription] = useState("");
    const [colorInput, setColorInput] = useState("")
    const [pictureURL, setPictureUrl] = useState("");
-   const [cantidadSizeSInput, setCantidadSizeS] = useState("")
-   const [cantidadSizeMInput, setCantidadSizeM] = useState("")
-   const [cantidadSizeLInput, setCantidadSizeL] = useState("")
-   const [cantidadSizeXLInput, setCantidadSizeXL] = useState("")
-   const [cantidadSizeXXLInput, setCantidadSizeXXL] = useState("")
+   const [cantidadInput, setCantidad] = useState("")
+   const [referenceInput, setReference] = useState("")
+
+   
 
  
  // errorMessages from BE
@@ -54,12 +53,9 @@ function CreateProduct() {
    const handleNameChange = (e) => setName(e.target.value);
    const handlePriceChange = (e) => setPrice(e.target.value);
    const handleDescriptionChange = (e) => setDescription(e.target.value);
-  //  const handleSizeChange = (e) => setSize(e.value);
-  const handleCantidadSizeSChange = (e) => setCantidadSizeS(e.target.value);
-  const handleCantidadSizeMChange = (e) => setCantidadSizeM(e.target.value);
-  const handleCantidadSizeLChange = (e) => setCantidadSizeL(e.target.value);
-  const handleCantidadSizeXLChange = (e) => setCantidadSizeXL(e.target.value);
-  const handleCantidadSizeXXLChange = (e) => setCantidadSizeXXL(e.target.value);
+   const handleSizeChange = (e) => setSize(e.value);
+  const handleCantidadChange = (e) => setCantidad(e.target.value);
+
   const handleColorChange = (e) => setColorInput(e.value);
   
 
@@ -95,13 +91,11 @@ function CreateProduct() {
       name: nameInput,
       price: priceInput,
       picture:pictureURL,
-      cantidadSizeS: cantidadSizeSInput,
-      cantidadSizeM: cantidadSizeMInput,
-      cantidadSizeL: cantidadSizeLInput,
-      cantidadSizeXL: cantidadSizeXLInput,
-      cantidadSizeXXL: cantidadSizeXXLInput,
+      size: sizeInput,
+      reference: referenceInput,
       description: descriptionInput,
       color: colorInput,
+      cantidad: cantidadInput,
      
      };
  
@@ -138,48 +132,28 @@ function CreateProduct() {
               Price
             </label>
           </div>
-          <div className="input-container">
-            <input value={cantidadSizeSInput} onChange={handleCantidadSizeSChange} />
-            <label className={cantidadSizeSInput && "filled"} htmlFor="Cantidad S">
-              Cantidad Talla S
-            </label>
-          </div>
-          
-        
-          <div className="input-container">
-            <input value={cantidadSizeMInput} onChange={handleCantidadSizeMChange} />
-            <label className={cantidadSizeMInput && "filled"} htmlFor="cantidad">
-              Cantidad Talla M 
-            </label>
-          </div>
-          <div className="input-container">
-            <input value={cantidadSizeLInput} onChange={handleCantidadSizeLChange} />
-            <label className={cantidadSizeLInput && "filled"} htmlFor="cantidad">
-              Cantidad Talla L 
-            </label>
-          </div>
-          <div className="input-container">
-            <input value={cantidadSizeXLInput} onChange={handleCantidadSizeXLChange} />
-            <label className={cantidadSizeXLInput && "filled"} htmlFor="cantidad">
-              Cantidad Talla XL 
-            </label>
-          </div>
-          <div className="input-container">
-            <input value={cantidadSizeXXLInput} onChange={handleCantidadSizeXXLChange} />
-            <label className={cantidadSizeXXLInput && "filled"} htmlFor="cantidad">
-              Cantidad Talla XXL
-            </label>
-          </div>
+ 
           <div className="input-container">
             <input
               value={descriptionInput}
               onChange={handleDescriptionChange}
             />
+            
             <label className={descriptionInput && "filled"} htmlFor="description">
               Description
             </label>
           </div>
-          {/* <div className="input-container">
+          <div className="input-container">
+            <input
+              value={descriptionInput}
+              onChange={handleCantidadChange}
+            />
+            
+            <label className={cantidadInput && "filled"} htmlFor="cantidad">
+              Cantidad
+            </label>
+          </div>
+          <div className="input-container">
             <div className="select-size">
             <div className='name-select'>
               <h4>Talla</h4>
@@ -194,7 +168,7 @@ function CreateProduct() {
 
             </div>
            
-          </div> */}
+          </div>
 
           <div className="input-container">
             <div className="select-size">
