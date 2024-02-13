2a85ff7cc47cf5acf2866f3421f600cc3694223d
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2024-02-13 10:01:57 +0100

    arrows insides carousel

--- src/components/track/Carousel.jsx
+++ src/components/track/Carousel.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../css/track/carousel.css"
+import rightArrow from "../../images/right-arrow.svg"
+import leftArrow from "../../images/left-arrow.svg"
 
 function Carousel({ track }) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -43,8 +45,12 @@ function Carousel({ track }) {
           }}
         />
       ))}
-      <button onClick={prevSlide} style={{ position: "absolute", top: "50%", left: "10px", zIndex: "1" }}>prev</button>
-      <button onClick={nextSlide} style={{ position: "absolute", top: "50%", right: "10px", zIndex: "1" }}>Next</button>
+      <button onClick={prevSlide} style={{ position: "absolute", top: "50%", left: "10px", zIndex: "1" }}>
+        <img src={leftArrow} alt="" />
+      </button>
+      <button onClick={nextSlide} style={{ position: "absolute", top: "50%", right: "10px", zIndex: "1" }}>
+        <img src={rightArrow} alt="" />
+      </button>
     </div>
   );
 }
